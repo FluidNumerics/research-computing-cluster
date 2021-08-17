8a8355bc8d59ef462bbb349ad4d8ed8c2da5231d
Joe Schoonover <joe@fluidnumerics.com>
2021-08-17 20:54:05 +0000

    FSG-642 Fix typos in  oneAPI repository specification

--- img/centos-intel/cluster-checker-setup.sh
+++ img/centos-intel/cluster-checker-setup.sh
@@ -3,12 +3,12 @@
 
 tee > /etc/yum.repos.d/oneAPI.repo << EOF
 [oneAPI]
-name=Intel® oneAPI repository
+name=Intel(R) oneAPI repository
 baseurl=https://yum.repos.intel.com/oneapi
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
-gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-23.PUB
+gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 EOF
 
 repoquery --repofrompath=reponame,https://yum.repos.intel.com/oneapi --repoid=reponame -a | grep clck
