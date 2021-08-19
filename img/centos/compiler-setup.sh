9d524de71cd6f675873b8a97dd5b544abdedb165
Joe Schoonover <joe@fluidnumerics.com>
2021-08-19 23:33:40 +0000

    FSG-664 Use string editing to correct the paths to amdclang/flang compilers

--- img/centos/compiler-setup.sh
+++ img/centos/compiler-setup.sh
@@ -10,21 +10,12 @@ source ${INSTALL_ROOT}/spack/share/spack/setup-env.sh
 
 # Set up AMD Clang compilers (provided by ROCm)
 spack compiler rm clang # Remove existing clang compiler
-cat << EOF >>  ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
-- compiler:
-    spec: clang@13.0.0
-    paths:
-      cc: /opt/rocm/bin/amdclang
-      cxx: /opt/rocm/bin/amdclang++
-      f77: /opt/rocm/bin/amdflang
-      fc: /opt/rocm/bin/amdflang
-    flags: {}
-    operating_system: centos7
-    target: x86_64
-    modules: []
-    environment: {}
-    extra_rpaths: []
-EOF
+
+# Adjust the paths to AMD Clang/Flang compilers
+sed -i "s#cc: /usr/bin/clang-ocl#cc: /opt/rocm/bin/amdclang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
+sed -i "s#cxx: None#cxx: /opt/rocm/bin/amdclang++#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
+sed -i "s#f77: None#f77: /opt/rocm/bin/amdflang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
+sed -i "s#fc: None#fc: /opt/rocm/bin/amdflang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
 
 ## Install "after-market" GNU compiler
 COMPILERS=("gcc@10.2.0"
@@ -33,6 +24,7 @@ COMPILERS=("gcc@10.2.0"
 
 for COMPILER in "${COMPILERS[@]}"; do
   spack install ${COMPILER} % gcc@4.8.5 target=${ARCH}
+  spack load ${COMPILER} && spack compiler find --scope site && spack unload ${COMPILER}
 done
 
 # Install OpenMPI with desired compilers
@@ -41,7 +33,6 @@ COMPILERS=("gcc@10.2.0"
            "clang"
 	   "intel")
 for COMPILER in "${COMPILERS[@]}"; do
-  spack load ${COMPILER} && spack compiler find --scope site && spack unload ${COMPILER}
   if [[ "$COMPILER" == *"intel"* ]];then
     spack install openmpi@4.0.5 % intel target=${ARCH}
   else
