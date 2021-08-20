809713c0e9805821610e1a37e494c597da4c502d
Joe Schoonover <joe@fluidnumerics.com>
2021-08-20 20:29:06 +0000

    Fix install script to bring in dmtcp through hpctools

---
+++ img/debian/hpctools-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#
+# Maintainers : @schoonovernumerics
+#
+# //////////////////////////////////////////////////////////////// #
+
+
+source ${INSTALL_ROOT}/spack/share/spack/setup-env.sh
+
+
+# Checkpoint/Restart tools
+spack install dmtcp target=${ARCH}
+
+# Profilers
+spack install hpctoolkit@2021.05.15 +cuda~viewer target=${ARCH}  # HPC Toolkit requires gcc 7 or above
+spack install intel-oneapi-vtune@2021.6.0 target=${ARCH}
+
+spack gc -y
+spack module lmod refresh --delete-tree -y
+
+
+# Benchmarks
+#
+#   hpcc - installs HPL, DGEMM, STREAM
+#
+spack install hpcc target=x86_64
+spack install hpcc target=cascadelake
+spack install hpcc target=zen3
+
+spack install hpcg target=x86_64
+spack install hpcg target=cascadelake
+spack install hpcg target=zen3
+
+spack install osu-micro-benchmarks target=x86_64
