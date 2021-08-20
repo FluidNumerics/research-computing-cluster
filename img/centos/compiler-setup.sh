456944af3e78e01b6fa73e93282656d1dc5fd67e
Joe Schoonover <joe@fluidnumerics.com>
2021-08-20 18:16:08 +0000

    FSG-664 Resolve issues with clang compiler in spack

--- img/centos/compiler-setup.sh
+++ img/centos/compiler-setup.sh
@@ -8,30 +8,31 @@
 
 source ${INSTALL_ROOT}/spack/share/spack/setup-env.sh
 
-# Set up AMD Clang compilers (provided by ROCm)
-spack compiler rm clang # Remove existing clang compiler
-
-# Adjust the paths to AMD Clang/Flang compilers
-sed -i "s#cc: /usr/bin/clang-ocl#cc: /opt/rocm/bin/amdclang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
-sed -i "s#cxx: None#cxx: /opt/rocm/bin/amdclang++#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
-sed -i "s#f77: None#f77: /opt/rocm/bin/amdflang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
-sed -i "s#fc: None#fc: /opt/rocm/bin/amdflang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
 
 ## Install "after-market" GNU compiler
-COMPILERS=("gcc@10.2.0"
-           "gcc@9.4.0"
-	   "intel-oneapi-compilers@2021.3.0")
+#COMPILERS=("gcc@10.2.0"
+#           "gcc@9.4.0"
+#	   "intel-oneapi-compilers@2021.3.0")
+#
+#for COMPILER in "${COMPILERS[@]}"; do
+#  spack install ${COMPILER} % gcc@4.8.5 target=${ARCH}
+#  spack load ${COMPILER} && spack compiler find --scope site && spack unload ${COMPILER}
+#done
 
-for COMPILER in "${COMPILERS[@]}"; do
-  spack install ${COMPILER} % gcc@4.8.5 target=${ARCH}
-  spack load ${COMPILER} && spack compiler find --scope site && spack unload ${COMPILER}
-done
+spack compiler find --scope site
+# Adjust the paths to AMD Clang/Flang compilers
+sed -i "s#cc: /bin/clang-ocl#cc: /opt/rocm/bin/amdclang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
+sed -i "s#cxx: null#cxx: /opt/rocm/bin/amdclang++#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
+sed -i "s#f77: null#f77: /opt/rocm/bin/amdflang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
+sed -i "s#fc: null#fc: /opt/rocm/bin/amdflang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
+cat ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
 
 # Install OpenMPI with desired compilers
-COMPILERS=("gcc@10.2.0"
-           "gcc@9.4.0"
-           "clang"
-	   "intel")
+#COMPILERS=("gcc@10.2.0"
+#           "gcc@9.4.0"
+#           "clang@13.0.0"
+#	   "intel")
+COMPILERS=("clang@13.0.0")
 for COMPILER in "${COMPILERS[@]}"; do
   if [[ "$COMPILER" == *"intel"* ]];then
     spack install openmpi@4.0.5 % intel target=${ARCH}
