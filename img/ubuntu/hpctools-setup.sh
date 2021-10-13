43d6a855195b5b1296b5b4c8061ed9fd3b0757e3
Joe Schoonover <joe@fluidnumerics.com>
2021-10-13 23:22:28 +0000

    Set debian and ubuntu images to build off foss images

--- img/ubuntu/hpctools-setup.sh
+++ img/ubuntu/hpctools-setup.sh
@@ -14,7 +14,6 @@ spack install dmtcp target=${ARCH}
 
 # Profilers
 spack install hpctoolkit@2021.05.15 +cuda~viewer target=${ARCH}  # HPC Toolkit requires gcc 7 or above
-spack install intel-oneapi-vtune@2021.6.0 target=${ARCH}
 
 spack gc -y
 spack module lmod refresh --delete-tree -y
