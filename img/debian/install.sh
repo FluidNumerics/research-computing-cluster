57f463ff897d02f48e76f20b006382a710420a44
Joe Schoonover <joe@fluidnumerics.com>
2021-10-26 03:51:34 +0000

    Add debian marketplace solution

--- img/debian/install.sh
+++ img/debian/install.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-
-function system_deps(){
-
-    export DEBIAN_FRONTEND=noninteractive
-    rm /var/lib/dpkg/lock
-    rm /var/lib/apt/lists/lock
-    rm /var/cache/apt/archives/lock
-    dpkg  --configure -a
-    apt-get update -y 
-    apt-get install -y libnuma-dev python3-dev python3-pip build-essential zip unzip
-    pip3 install --upgrade google-cloud-storage google-api-python-client oauth2client google-cloud \
-    	               cython pyyaml parse docopt jsonschema dictdiffer
-}
-
 function cluster_services_setup(){
 
     mkdir -p /apps/cls/build
@@ -36,27 +22,10 @@ function cluster_services_setup(){
     echo "export PATH=\${PATH}:/apps/cls/bin" >> /etc/profile.d/z11_cls.sh
 }
 
-function rocm_setup(){
-    wget -q -O - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -
-    echo 'deb [arch=amd64] https://repo.radeon.com/rocm/apt/4.2/ xenial main' | sudo tee /etc/apt/sources.list.d/rocm.list
-    apt-get update -y
-    apt-get install -y rocm-dev
-    
-    echo 'ADD_EXTRA_GROUPS=1' | sudo tee -a /etc/adduser.conf
-    echo 'EXTRA_GROUPS=video' | sudo tee -a /etc/adduser.conf
-    
-    cat > /etc/profile.d/z11-rocm.sh << EOL
-#!/bin/bash
-
-export PATH=\${PATH}:/opt/rocm/bin
-export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/opt/rocm/lib
-export HIP_PLATFORM=nvcc
-
-EOL
-}
-
-system_deps
-
 cluster_services_setup
 
-#rocm_setup
+if [[ -n "$SPACK_BUCKET" ]]; then
+    spack mirror rm RCC
+fi
+
+cat /dev/null > /var/log/syslog
